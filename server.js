c68d9c86c6132292a01f584e1c6f37df08c57b83
IGORAN <iavdeev758@gmail.com>
2025-06-02 23:40:41 +0500

    v3

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-javascript
 const express = require('express');
 const ytdl = require('ytdl-core');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -30,5 +29,5 @@ app.get('/download', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Сервер запущен на http://localhost:${port}`);
+    console.log(`Сервер запущен на порту ${port}`);
 });
